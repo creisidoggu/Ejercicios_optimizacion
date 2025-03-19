4cdae693d34f6434bf1dba2fd684ae96089ebf4a
creisidoggu <franfernandez299@gmail.com>
2025-03-19 18:09:32 +0100

    feat: :sparkles: Create initial population created

--- genetics/genetics.py
+++ genetics/genetics.py
@@ -1,4 +1,5 @@
 import json
+import random
 # Added some more variables of the items if i want to use it for my dnd page, but for this problem i'll use only weight and value
 path = "genetics/data/items.json"
 
@@ -13,12 +14,17 @@ def establish_individuals(file):
 
     return [{ "weight": obj["weight"], "value": obj["value"] } for obj in json_data]
 
-def create_population(population_length = 60):
-    # The objetive is to generate a binary code with the length of the individual (establish_individuals)
-    pass
+def create_population(individual_size, population_length = 60):
+    population = []
+    for individual in range(population_length):
+        current_individual = []
+        for gene in range(individual_size):
+            current_individual.append(random.randrange(0,2))
+        population.append(current_individual)
+    return population
 
 def main():
-    print(establish_individuals(path))
+    print(create_population(len(establish_individuals(path))))
 
 if __name__ == '__main__':
     main()
